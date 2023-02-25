78ddf392d6f86063ead64e98476e5dfcdb3a3a68
amckeon17 <amckeon17@hotmail.com>
2023-02-25 12:04:41 -0800

    Starting

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 // Assignment Code
+// Created variables and prompts
 var generateBtn = document.querySelector("#generate");
 function generatePassword () {
-  var chooseLower = confirm ("Do you want lowercase characters?")
-  var chooseCharacters = prompt ("How many characters do you want between 8 and 128?")
-  var chooseLowercase = confirm ("Do you want lowecase characters?")
-  var chooseUppercase = confirm ("Do you want uppercase characters?")
-  var chooseNumeric = confirm ("Do you want numbers?")
-  var chooseSpecial = confirm ("Do you want special characters?")
+  var chooseCharacters = prompt ("How many characters do you want between 8 and 128?");
+  var chooseLower = confirm ("Do you want lowercase characters? OK - yes Cancel - no");
+  var chooseUppercase = confirm ("Do you want uppercase characters? OK - yes Cancel - no");
+  var chooseNumeric = confirm ("Do you want numbers? OK - yes Cancel - no");
+  var chooseSpecial = confirm ("Do you want special characters? OK - yes Cancel - no");
 }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -16,6 +17,10 @@ function writePassword() {
   passwordText.value = password;
 
 }
+var choiceMade = " ";
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
